4aeeeb1225662289517326f04f155da7d06b0352
Avalanching <18269285634@163.com>
2019-05-05 12:06:44 +0800

    提交

---
+++ CocoaPodsDemo/Classes/AvaDemo.h
@@ -0,0 +1,16 @@
+//
+//  AvaDemo.h
+//  CocoaPodsDemo
+//
+//  Created by Avalanching on 2019/5/5.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface AvaDemo : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ CocoaPodsDemo/Classes/AvaDemo.m
@@ -0,0 +1,16 @@
+//
+//  AvaDemo.m
+//  CocoaPodsDemo
+//
+//  Created by Avalanching on 2019/5/5.
+//
+
+#import "AvaDemo.h"
+
+@implementation AvaDemo
+
++ (void)load {
+    NSLog(@"I'm  AvaDemo 🚀");
+}
+
+@end
